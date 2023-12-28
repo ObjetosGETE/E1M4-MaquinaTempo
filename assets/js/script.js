d073211f18c50656b229450be8a5964da0454e1f
RenataLimaTED <relferreira@senacrs.com.br>
2023-12-28 16:59:44 -0300

    animacao inicial

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+function escalaProporcao(largura, altura) {
+
+    var larguraScreen = $(window).width();
+    var alturaScreen = $(window).height();
+    var proporcaoAltura = (alturaScreen * 100) / altura;
+    var proporcaoLargura = (larguraScreen * 100) / largura;
+    var proporcao, larguraAltura, larguraAlturaAuto;
+
+    if (proporcaoAltura < proporcaoLargura) {
+        larguraAltura = "height";
+        larguraAlturaAuto = "width";
+        proporcao = proporcaoAltura / 100;
+    } else {
+        larguraAltura = "width";
+        larguraAlturaAuto = "height";
+        proporcao = proporcaoLargura / 100;
+    }
+
+    return [proporcao, larguraAltura, larguraAlturaAuto];
+}
+
+function resizeBodyConteudo() {
+
+    var proporcao1920 = escalaProporcao(1920, 1080)[0];
+
+    $(".conteudo").css({
+        "transform": "scale(" + proporcao1920 + ")",
+        "transform-origin": "center center"
+    });
+
+    var proporcao900;
+
+    if ($(window).width() < 992) {
+        proporcao900 = escalaProporcao(900, 576)[0];
+    } else {
+        proporcao900 = 1;
+    }
+}
+
+$(document).ready(function () {
+    resizeBodyConteudo()
+    $(window).resize(function () {
+        resizeBodyConteudo()
+    })
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".peca-top, .peca-teclado, .peca-left, .peca-right, .peca-left-teclado, .peca-left-teclado-2, .peca-right-teclado");
+    const animationContainer = document.querySelector(".animation-container");
+    const delayBetweenAnimations = 1000;
+
+    let index = 0;
+
+    function playNextAnimation() {
+        if (index < elements.length) {
+            elements[index].style.animationPlayState = "running";
+            index++;
+            setTimeout(playNextAnimation, delayBetweenAnimations);
+        }
+    }
+
+    playNextAnimation();
+});
