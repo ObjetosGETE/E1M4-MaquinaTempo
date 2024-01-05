6e90f564d5a616ce9e14cfb460adbd0e90ee8044
RenataLimaTED <116093219+RenataLimaTED@users.noreply.github.com>
2024-01-05 17:40:11 -0300

    ajuste animacao vortice, movimentos de pecas e ajuste no som

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,6 +83,8 @@ $(document).ready(function () {
             musica.pause();
             musica.currentTime = 0;
         }
+
+
     }
 
     function pausarTodasMusicas() {
@@ -100,48 +102,160 @@ $(document).ready(function () {
         musica2000.currentTime = 0;
     }
 
+    var campoText = '';
 
+    function enviarTextoInput() {
+        $('#texto').val(campoText);
+    }
 
-    function digitacao() {
-        var campoText = '';
+    $('.btn-teclado').on('click', function () {
+        var buttonText = $(this).text();
 
-        function enviarTextoInput() {
-            $('#texto').val(campoText);
+        if (this.id === 'es') {
+            campoText += ' ';
+        } else if (this.id === 'del') {
+            campoText = campoText.slice(0, -1);
+        } else {
+            campoText += buttonText;
         }
 
-        $('.btn-teclado').on('click', function () {
-            var buttonText = $(this).text();
+        enviarTextoInput();
+    });
 
-            if (this.id === 'es') {
-                campoText += ' ';
-            } else if (this.id === 'del') {
-                campoText = campoText.slice(0, -1);
-            } else {
-                campoText += buttonText;
-            }
+    function efeitoDeDigitacao() {
+        function typeWrite(elemento) {
+            const textoArray = elemento.innerHTML.split('');
+            elemento.innerHTML = ' ';
+            textoArray.forEach(function (letra, i) {
 
-            enviarTextoInput();
-        });
+                setTimeout(function () {
+                    elemento.innerHTML += letra;
+                }, 75 * i);
+
+            });
+        }
+        const titulo = document.querySelector('.titulo-principal');
+        typeWrite(titulo);
     }
 
     function limparCampoTexto() {
         $("#texto").val('');
+        campoText = '';
+    }
+
+    const elements = $(".peca-top, .peca-left, .peca-right");
+    const animationContainer = $(".animation-container");
+    const delayBetweenAnimations = 1000;
+
+    let index = 0;
+
+    function playNextAnimation() {
+        if (index < elements.length) {
+            $(elements[index]).css("animation-play-state", "running");
+            index++;
+            setTimeout(playNextAnimation, delayBetweenAnimations);
+        }
+    }
+
+    function pecasVoltando() {
+        function voltaTopo() {
+            $("#p-top").removeClass('peca-top');
+            $("#p-top").addClass('volta-topo');
+        }
+
+        function voltaTeclado() {
+            $("#p-teclado").removeClass('peca-teclado');
+            $("#p-teclado").addClass('volta-peca-teclado');
+        }
+
+        function voltaPecaLeft() {
+            $("#p-left").removeClass('peca-left');
+            $("#p-left").addClass('volta-peca-left');
+        }
+
+        function voltaPecaRigth() {
+            $("#p-rigth").removeClass('peca-rigth');
+            $("#p-rigth").addClass('volta-peca-right');
+        }
+
+        function voltaPecaLeftTeclado() {
+            $("#p-teclado-left").removeClass('peca-left-teclado');
+            $("#p-teclado-left").addClass('volta-left-teclado');
+        }
+
+        function voltaPecaLeftTeclado2() {
+            $("#p-teclado-left2").removeClass('peca-left-teclado-2');
+            $("#p-teclado-left2").addClass('volta-left-teclado-2');
+        }
+
+        function voltaPecaRigthTeclado2() {
+            $("#p-teclado-rigth2").removeClass('peca-right-teclado');
+            $("#p-teclado-rigth2").addClass('volta-right-teclado');
+        }
+
+
+        voltaTopo();
+        voltaTeclado();
+        voltaPecaLeft();
+        voltaPecaRigth();
+        voltaPecaLeftTeclado();
+        voltaPecaLeftTeclado2();
+        voltaPecaRigthTeclado2();
+    }
+
+    function posicaoInicial() {
+
+        $("#p-top").addClass('peca-top');
+        $("#p-top").removeClass('volta-topo');
+
+        $("#p-teclado").addClass('peca-teclado');
+        $("#p-teclado").removeClass('volta-peca-teclado');
+
+        $("#p-left").addClass('peca-left');
+        $("#p-left").removeClass('volta-peca-left');
+
+        $("#p-rigth").addClass('peca-rigth');
+        $("#p-rigth").removeClass('volta-peca-right');
+
+        $("#p-teclado-left").addClass('peca-left-teclado');
+        $("#p-teclado-left").removeClass('volta-left-teclado');
+
+        $("#p-teclado-left2").addClass('peca-left-teclado-2');
+        $("#p-teclado-left2").removeClass('volta-left-teclado-2');
+
+        $("#p-teclado-rigth2").addClass('peca-right-teclado');
+        $("#p-teclado-rigth2").removeClass('volta-right-teclado');
+
     }
 
     function btnVoltar() {
         $('.btn-voltar').on('click', function () {
+            limparCampoTexto();
             $('.anos30, .anos70, .anos80, .anos90, .anos00').addClass('d-none');
             $('#vortice').addClass('d-none');
-            $('.maquina-tempo').removeClass('d-none');
+            $('#btnValidarAno').css({
+                display: 'block'
+            });
+            setTimeout(function () {
+                $('#h1-maquina').css({
+                    display: 'block'
+                });
+                efeitoDeDigitacao();
+            }, 4000);
+
+
             $("#texto").fadeIn(3000);
             $("#bgpadrao").fadeIn(1000);
             pausarTodasMusicas();
-            limparCampoTexto();
+
+            posicaoInicial();
         });
     }
 
     function vorticeNaTela() {
-
+        $('#h1-maquina').css({
+            display: 'none'
+        });
         $("#texto").fadeOut(1000);
         $("#bgpadrao").fadeOut(2000);
         $("#btnValidarAno").fadeOut(2000);
@@ -158,13 +272,14 @@ $(document).ready(function () {
                 })
 
                 .animate({
-                    top: '266px',
-                    left: '263px',
-                    width: '378px'
-                }, 2000);
+                    top: '0px',
+                    left: '0px',
+                    round: '1%',
+                    width: '100%'
+                }, 3000);
         }, 3000);
-    }
 
+    }
 
     function validacaoAno() {
         var textoCompleto = $('#texto').val().trim();
@@ -174,34 +289,57 @@ $(document).ready(function () {
         if (matchAno) {
             var doisUltimosDigitos = matchAno[0];
 
-            // $('.maquina-tempo').addClass('d-none');
-
             switch (doisUltimosDigitos) {
                 case '30':
+                    setTimeout(function () {
+                        $('#musica30')[0].play();
+                        $('.meuBotao i').text('volume_up');
+                    }, 3000);
+                    pecasVoltando();
                     vorticeNaTela();
                     setTimeout(function () {
                         $('.anos30').removeClass('d-none');
-                    }, 5000);
+                    }, 8000);
                     break;
                 case '70':
+                    setTimeout(function () {
+                        $('#musica70')[0].play();
+                        $('.meuBotao i').text('volume_up');
+                    }, 3000);
+                    pecasVoltando();
                     vorticeNaTela();
                     setTimeout(function () {
                         $('.anos70').removeClass('d-none');
                     }, 5000);
                     break;
                 case '80':
+                    setTimeout(function () {
+                        $('#musica80')[0].play();
+                        $('.meuBotao i').text('volume_up');
+                    }, 3000);
+                    pecasVoltando();
                     vorticeNaTela();
                     setTimeout(function () {
                         $('.anos80').removeClass('d-none');
                     }, 5000);
                     break;
                 case '90':
+                    setTimeout(function () {
+                        $('#musica90')[0].play();
+                        $('.meuBotao i').text('volume_up');
+                    }, 3000);
+                    pecasVoltando();
                     vorticeNaTela();
                     setTimeout(function () {
                         $('.anos90').removeClass('d-none');
                     }, 5000);
                     break;
                 case '00':
+                    setTimeout(function () {
+                        $('#musica2000')[0].play();
+                        $('.meuBotao i').text('volume_up');
+                    }, 3000);
+                    pecasVoltando();
                     vorticeNaTela();
                     setTimeout(function () {
                         $('.anos00').removeClass('d-none');
@@ -216,6 +354,7 @@ $(document).ready(function () {
     }
 
 
+
     $('#btnValidarAno').on('click', function () {
         validacaoAno();
     });
@@ -224,23 +363,10 @@ $(document).ready(function () {
         resizeBodyConteudo();
     });
 
-    const elements = $(".peca-top, .peca-left, .peca-right");
-    const animationContainer = $(".animation-container");
-    const delayBetweenAnimations = 1000;
-
-    let index = 0;
-
-    function playNextAnimation() {
-        if (index < elements.length) {
-            $(elements[index]).css("animation-play-state", "running");
-            index++;
-            setTimeout(playNextAnimation, delayBetweenAnimations);
-        }
-    }
-
+    efeitoDeDigitacao();
     playNextAnimation();
     btnVoltar();
     audioClick();
-    digitacao();
+    enviarTextoInput();
     resizeBodyConteudo();
 });
